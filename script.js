ab142fa17d654cdf9f3944cc624cb7bf0cbb3d51
Valerie Kucher <passechnik214@gmail.com>
2022-12-13 00:46:23 +0200

    add factorial task

---
+++ script.js
@@ -0,0 +1,25 @@
+//функція, яка виводить кількість переданих нею аргументів
+function myFunction(a, b, c) {
+    console.log(arguments.length)
+}
+
+//функція, яка приймає два числа і повертає: -1 - якщо перше число менше за друге; 1 - якщо перше число більше за друге; 0 - якщо числа рівні
+function myNumbers (a, b) {
+    if (a < b) {
+        console.log('-1');
+    } else if (a > b) {
+        console.log('1');
+    } else if (a === b) {
+        console.log('0');
+    }
+}
+
+//функція, яка обчислює факторіал переданого їй числа (рекурсія)
+function factorial(a) {
+    if (a == 1) {
+        return 1
+    } else {
+        return a * factorial(a - 1)
+    }
+}
+factorial(6)
